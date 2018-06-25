d1bcd631921d099d8f29da3d99675281b44fe40d
trentdurfee <trentd_01@yahoo.com.au>
2018-06-25 22:29:19 +1000

    Modified Prototype based on discussions / new ideas
    After discussions decided for now on the latest prototype following changes:
    
    - removed side commenting boxes for now. Posed a very difficult design challenge (was expected as very different way of thinking) and for now couldn't figure out a functional way forward.
    
    - Added left side 'related linked news stories' these are related to whatever main story is being viewed in the middle section of the site.
    
    - Added to right hand side, real-time trending user comments (sample data for now, hard-coded not from DB).
    
    - Added real-time ticket to top of page, this would show in real-time users comments being added that again are related to the article being viewed in the middle section.
    
    - Code fixed up and clean-ups in areas.
    
    - Code fixed up and resolved for 'topic headings' currently however only 'polotics' works, code is there to support others just need to add the correct 'feeds' / 'DB data (once DB is connected.

--- fuel/app/views/initialise/index.php
+++ fuel/app/views/initialise/index.php
@@ -5,7 +5,23 @@
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
 	<meta charset="utf-8">
 	<title>The Converse</title>
-	<?php echo Asset::css('bootstrap.css'); ?>
+	<?php echo Asset::css(array('bootstrap.css', 'scroller-style.css')); ?>
+    <?php echo Asset::js(array('jquery.totemticker.min.js')); ?>
+   <!--<?php //echo Asset::img(array('demo-bg.jpg')); ?>-->
+
+   <script type="text/javascript">
+		$(function(){
+			$('#user-related-linked-comments').totemticker({
+				row_height	:	'200px',
+				next		:	'#ticker-next',
+				previous	:	'#ticker-previous',
+				stop		:	'#stop',
+				start		:	'#start',
+				mousestop	:	true,
+			});
+		});
+	</script>
+
 	<style>
 		#logo{
 			display: block;
@@ -89,6 +105,7 @@
 		body {
 			margin: 0px 0px 0px 0px; 
 			min-height:100%;
+			background-color: #fafaff;
 		}
 
 		.commentboxcoloring {
@@ -108,6 +125,86 @@
 			cursor: pointer;
 		}
 
+		.topnav-scrollable-comments {
+			background-color: #fafaff;
+			color: #883ced;
+			border: none;
+			text-align: center;
+			cursor: pointer;
+			display: inline-block;
+			width: auto;
+			white-space: nowrap;
+			padding: 0 3rem;
+      		font-size: 2rem;
+			-webkit-animation-iteration-count: infinite; 
+            animation-iteration-count: infinite;
+    			-webkit-animation-timing-function: linear;
+            animation-timing-function: linear;
+   				-webkit-animation-name: topnav-scrollable-comments;
+           animation-name: topnav-scrollable-comments;
+    			-webkit-animation-duration: 30s;
+            animation-duration: 30s;
+			/*white-space:nowrap;*/
+		}
+
+		 @keyframes topnav-scrollable-comments {
+  			0% {
+			    -webkit-transform: translate3d(0, 0, 0);
+			    transform: translate3d(0, 0, 0);
+			    visibility: visible;
+  			}
+
+  			100% {
+			    -webkit-transform: translate3d(-100%, 0, 0);
+			    transform: translate3d(-100%, 0, 0);
+  			}
+		}
+
+		.realtimecomment-nav {
+			margin: 0;
+			padding: 0;
+			justify-content: center;
+			height: 50px;
+			width: auto;
+			overflow-x: auto;
+			list-style: none;
+			white-space: nowrap;
+			/*white-space:nowrap;*/
+		}
+
+		#usercomments-linked {
+			overflow: hidden;
+			height: 1500px;
+    		overflow:hidden;
+    		margin:0;
+    		padding:0;
+    		-webkit-box-shadow:0 1px 3px rgba(0,0,0,.4) 
+		}
+
+		#usercomments-linked li {
+		    padding:35px 20px;
+		    display:block;
+		    background: #66ccff;
+		    color:#333;
+		    border-bottom:1px solid #ddd;
+		    text-align:center;
+		    font-size:25px;
+		    font-weight:bold;
+		    font-family:Helvetica Neue,times,serif
+		}
+
+		.realtimecommnet-nav -webkit-scrollbar {
+  			display: none;
+		}
+
+		.realtimecommenttext {
+			color: #883ced;
+    		width: 300px;
+    		height: 140px;
+    		white-space: nowrap;
+
+		}
+
 		.topicbtns:hover{
 			background-color: #0066CC;
 			color: #EEEEEE;
@@ -120,6 +217,22 @@
 			width: 100%;
 			margin: 0;
 			padding: 0;
+		}
+
+		.related-news-linked-recent {
+			height: 220px;
+			background-color: #cccccc; /* just for now so can see the sizing / spacing etc */
+			padding-top: 5px;
+			padding-bottom: 5px;
+			margin-top: 20px;
+		}
+
+		.related-news-linked-usercomments {
+			height: 1500px;
+			/*background-color: #66ccff;*/ /* just for now so can see the sizing / spacing etc */
+			padding-top: 5px;
+			padding-bottom: 5px;
+			margin-top: 20px;
 		}
 
 	</style>
@@ -151,6 +264,7 @@
         });
     });
 });
+
 </script>
 <script>
 
@@ -186,12 +300,11 @@
         	}
     	});
 	}
-
 </script>
 	<header>
 		<h1 class="setheading">The Converse</h1>
 		<p class="setheading" style="text-align:center;padding-top: 10px;width:100%;">The place for constructive conversation and fun debating of any topic! Be kind to your fellow human.</p>
-		<p style="padding-left: 31.5%;padding-right: 31.5%;"><a class="btn btn-primary btn-lg" style="margin-right: auto;margin-left: 35%;" href="">Read the Mission Statement</a></p>
+		<p style="padding-left: 31.5%;padding-right: 31.5%;"><a class="btn btn-primary btn-lg" style="margin-right: auto;" href="">Read the Mission Statement</a></p>
 	</header>
 
 	<!--
@@ -276,12 +389,59 @@
 			<button type="button" class="topicbtns">ENTERTAINMENT & ART DISCUSSION</button>
 			<button type="button" class="topicbtns">OPINION PIECES</button>-->
 		</div>
+
+		<div class="row-fluid" style="padding-left: 5%;padding-right: 5%;">
+		<div class="realtimecomment-nav">
+				<div class="" id="scrolling-comment-ticker">
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 1 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 2 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 3 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 4 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 5 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 6 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 7 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 8 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 9 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 10 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 11 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 12 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 13 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 14 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>																																							<div class="topnav-scrollable-comments">Sample Real-time Comment 15 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>	
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 16 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>		
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 17 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>		
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 18 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>		
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 19 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>		
+					<div class="topnav-scrollable-comments">Sample Real-time Comment 20 heres a lot of text to test out the size of the real time comments as want to be able to read enough to entice someone to click/hover over it. Will also need a nice UX-wise style to see the whole comment and maybe even comment inline in agreement or very least positvely/negatively add 'karma' to it</div>
+				</ul>
+			</div>
+		</div>
   		<div class="row-fluid" style="height:7676px;">
     		<div class="span2 leftparty" style="height:100%;">
       			<!--Left Sidebar content-->
-      			<div id="commenttextbox" contenteditable="true" style="padding:10px;width:80%;padding-left: 5%;padding-right: 5%;margin-top: 5%; color: #333333;height:150px;" class="center commentboxcoloring">
-					Add Comment with a Different opinion to main topic:
-				</div>
+      			<div id="commenttextbox" contenteditable="false" style="padding:10px;width:80%;padding-left: 5%;padding-right: 5%;margin-top: 5%; color: #333333;height:1500px;" class="center commentboxcoloring">
+						<div class="related-news-linked-recent">
+							<p style="color:#333333;">The Stock Market Loophole That Screws the Little Guy</p>
+							<img src="<?php echo "assets/img/ideas-test-img.jpg" ?>" style="width:200px;height:100px;padding-left: 15px;float: left;" id="news-related-thumb" />
+							<p style="text-align: justify;text-indent: 2em;color:#333333;">THE AMERICAN DREAM is the small-business dream: Work hard and achieve success. But the American promise is one of shared prosperity, as encapsulated in the idea that “what is good for General Motors is good for America.”</p>
+							
+
+						</div>
+						<div class="related-news-linked-recent">
+							<img src="" id="news-related-thumb" />
+						</div>
+						<div class="related-news-linked-recent">
+							<img src="" id="news-related-thumb" />
+						</div>
+						<div class="related-news-linked-recent">
+							<img src="" id="news-related-thumb" />
+						</div>
+						<div class="related-news-linked-recent">
+							<img src="" id="news-related-thumb" />
+						</div>
+						<div class="related-news-linked-recent">
+							<img src="" id="news-related-thumb" />
+						</div>
+					</div>
     		</div>
    			<div class="span8 jumbotron offset2">
      			 <!--Body content-->
@@ -289,14 +449,44 @@
 				</div>
     		</div>
 			<div style="height: 100%;" class="row-fluid">
-				<div class="rightparty span2 ">
+				<div class="rightparty span2" id="usercomments-linked">
       			<!--Right Sidebar content-->
-      				<div id="commenttextbox" contenteditable="true" style="padding:10px;width:80%;padding-left: 5%;padding-right: 5%;margin-top: 5%; color: #333333;height:150px;" class="center commentboxcoloring">
-						Add Comment with a Different opinion to main topic:
+      				<div id="commenttextbox" contenteditable="false" style="padding:10px;width:80%;padding-left: 5%;padding-right: 5%;margin-top: 5%; color: #333333;height:1500px;" class="center commentboxcoloring">
+						<ul class="related-news-linked-usercomments">
+							<li><p>Sample 1 User comment</p></li>
+							<li><p>Sample 2 User comment</p></li>
+							<li><p>Sample 3 User comment</p></li>
+							<li><p>Sample 4 User comment</p></li>
+							<li><p>Sample 5 User comment</p></li>
+							<li><p>Sample 6 User comment</p></li>
+							<li><p>Sample 7 User comment</p></li>
+							<li><p>Sample 8 User comment</p></li>
+							<li><p>Sample 9 User comment</p></li>
+							<li><p>Sample 10 User comment</p></li>
+							<li><p>Sample 11 User comment</p></li>
+							<li><p>Sample 12 User comment</p></li>
+							<li><p>Sample 13 User comment</p></li>
+							<li><p>Sample 14 User comment</p></li>
+							<li><p>Sample 15 User comment</p></li>
+							<li><p>Sample 16 User comment</p></li>
+							<li><p>Sample 17 User comment</p></li>
+							<li><p>Sample 18 User comment</p></li>
+						</ul>
 					</div>
     			</div>
 			</div>
   		</div>
 	</div>
+			<script type="text/javascript">
+			
+		$('.related-news-linked-usercomments').totemticker({
+			row_height  :   '400px',
+			next        :   '#ticker-next',
+			previous    :   '#ticker-previous',
+			stop        :   '#stop',
+			start       :   '#start',
+		});
+
+		</script>
 </body>
 </html>
